dd0aa621eb7555129e796b1cd7bed0d700cd45b4
Shahar Best <bestshahar9@gmail.com>
2022-06-08 00:23:29 +0300

    remove junk

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { Outlet } from "react-router-dom";
 import { Footer } from "./Components/Footer";
 import { Header } from "./Components/Header";
