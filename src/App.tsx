c8079de401e4035278695cd697dd6369fcbc0181
Shahar Best <bestshahar9@gmail.com>
2022-06-07 15:15:20 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { Home } from "./Components/Home";
+
+function App() {
+  return <div className="App"><Home /></div>;
+}
+
+export default App;
---
+++ src/Components/Contact.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Contact() {
+  return (
+    <div>
+      <h1>contact</h1>
+    </div>
+  );
+}
